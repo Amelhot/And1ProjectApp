764c2998a39cb295cb48ea1b0a9f0675e978f7ed
Brain321 <amelhot2000@gmail.com>
2022-12-04 05:31:05 +0100

    Created NoteInfoActivity and implemented methods.
    Every added note will be sent and stored in Firebase Database.

---
+++ app/src/main/java/com/example/smartnoteapp/NoteInfoActivity.java
@@ -0,0 +1,75 @@
+package com.example.smartnoteapp;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.speech.tts.UtteranceProgressListener;
+import android.widget.EditText;
+import android.widget.ImageButton;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.Timestamp;
+import com.google.firebase.firestore.DocumentReference;
+
+public class NoteInfoActivity extends AppCompatActivity {
+
+    EditText titleEditText;
+    EditText contentEditText;
+    ImageButton saveNoteBtn;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_note_info);
+
+        titleEditText = findViewById(R.id.noteTitleTxt);
+        contentEditText = findViewById(R.id.noteContentTxt);
+        saveNoteBtn = findViewById(R.id.checkSaveBtn);
+
+
+        saveNoteBtn.setOnClickListener((v)-> saveNote());
+
+
+    }
+
+    void saveNote(){
+        String noteTitle = titleEditText.getText().toString();
+        String noteContent = contentEditText.getText().toString();
+        if (noteTitle == null || noteTitle.isEmpty() ){
+            titleEditText.setError("Title is needed");
+            return;
+        }
+        Note note = new Note();
+        note.setTitle(noteTitle);
+        note.setContent(noteContent);
+        note.setTimestamp(Timestamp.now());
+
+        saveNoteToFirebase(note);
+    }
+
+    void saveNoteToFirebase(Note note){
+        DocumentReference documentReference;
+        documentReference = Utility.getCollectionReferenceForNotes().document();
+
+        documentReference.set(note).addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                if (task.isSuccessful()) {
+                    //Added note
+                    Utility.showToast(NoteInfoActivity.this, "Notes Added!");
+                    finish();
+
+                }else{
+                    Utility.showToast(NoteInfoActivity.this,"Adding note failed");
+                }
+
+            }
+        });
+
+    }
+
+
+}
